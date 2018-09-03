610814eb2812109cc353e3e6652802945fd7dc81
HenriqueDMahl <42521129+HenriqueDMahl@users.noreply.github.com>
2018-09-03 14:22:04 -0300

    Add files via upload

--- JAVASCRIPT/funcoes.js
+++ JAVASCRIPT/funcoes.js
@@ -1,9 +1,9 @@
 /**
- * 
- * 
+ *
+ *
  * EXERCICIO 6
- * 
- * 
+ *
+ *
  */
 
 function somaNumeros(){
@@ -29,8 +29,8 @@ function matematica(){
     quad.push(Math.pow(i,2));
     cubo.push(Math.pow(i,3));
   }
-  document.getElementById("mat").innerHTML = "Fatorial: " + fat + "<br>" + 
-  "Quadrado: " + quad + "<br>" + 
+  document.getElementById("mat").innerHTML = "Fatorial: " + fat + "<br>" +
+  "Quadrado: " + quad + "<br>" +
   "Cubo: " + cubo + "<br>";
 }
 
@@ -63,7 +63,7 @@ function CheckDate() {
 	if (mes == 4 || mes == 6 || mes == 9 || mes == 11 && dia > 30){
     aRet = false;
   }
-	else{ 
+	else{
 	  if ((ano % 4) != 0 && mes == 2 && dia > 28){
       aRet = false;
     } else {
@@ -72,9 +72,9 @@ function CheckDate() {
         }
       }
     }
-  } else { 
+  } else {
     aRet = false;
-  }  
+  }
 
   if(aRet){
     alert('Data Valida');
@@ -87,15 +87,21 @@ function checkMail(){
   var mail = document.getElementById('t2').value;
   var er = new RegExp(/^[A-Za-z0-9_\-\.]+@[A-Za-z0-9_\-\.]{2,}\.[A-Za-z0-9]{2,}(\.[A-Za-z0-9])?/);
   if(typeof(mail) == "string"){
-    if(er.test(mail)){ 
+    if(er.test(mail)){
       alert('Email Valido');
       return;
-    }	
+    }else{
+      alert('Email Invalido');
+      return;
+    }
   }else if(typeof(mail) == "object"){
     if(er.test(mail.value)){
       alert('Email Valido');
       return;
-    }	
+    }else{
+      alert('Email Invalido');
+      return;
+    }
   }else{
     alert('Email Invalido');
     return;
@@ -111,20 +117,20 @@ if (strCPF == "00000000000")  {
   alert('CPF invalido');
   return;
 }
-   
+
 for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
 Resto = (Soma * 10) % 11;
- 
+
   if ((Resto == 10) || (Resto == 11))  Resto = 0;
   if (Resto != parseInt(strCPF.substring(9, 10)) ) {
     alert('CPF invalido');
     return;
   }
- 
+
 Soma = 0;
   for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
   Resto = (Soma * 10) % 11;
- 
+
   if ((Resto == 10) || (Resto == 11))  Resto = 0;
   if (Resto != parseInt(strCPF.substring(10, 11) ) ) {
     alert('CPF invalido');
@@ -135,11 +141,11 @@ Soma = 0;
 
 
 /**
- * 
- * 
+ *
+ *
  * EXERCICIO 5
- * 
- * 
+ *
+ *
  */
 
 
