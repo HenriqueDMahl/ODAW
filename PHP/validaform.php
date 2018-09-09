4b9eb2ad63d9536e5aaa47e6f1168ccb63a14954
HenriqueDMahl <42521129+HenriqueDMahl@users.noreply.github.com>
2018-09-08 23:58:24 -0300

    Add files via upload

--- PHP/validaform.php
+++ PHP/validaform.php
@@ -1,10 +1,21 @@
-<html>
-<body>
-
-TESTE!!!!
 <?php
 // define variables and set to empty values
-$nameErr = $senhaErr = $postgetErr = $feedErr = $check = "";
+$nameErr = $senhaErr = $postgetErr = $feedErr = $checkErr = $selectErr = "";
+$name = $senha = $postget = $feed = $check = $select = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST["nome"];
+  $senha = $_POST["senha"];
+  $feed = $_POST["feed"];
+  $select = $_POST["selected"];
+  $postget = $_POST["s"];
+}else{
+  $name = $_GET["nome"];
+  $senha = $_GET["senha"];
+  $feed = $_GET["feed"];
+  $select = $_GET["selected"];
+  $postget = $_GET["s"];
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
  if (empty($_POST["nome"])) {
@@ -19,15 +30,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
    $feedErr = "Feed invalido";
  }
 
- if (isset($_POST["select"])) {
+ if (isset($_POST["selected"])) {
    $selectErr = "Select invalido";
  }
 
  if (!(isset($_POST["s"]))) {
    $postgetErr = "Radio invalido";
- }
- if (!(isset($_POST["check"]))){
-     $check = "Check invalido";
  }
 }else{
  if (empty($_GET["nome"])) {
@@ -42,33 +50,51 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
    $feedErr = "Feed invalido";
  }
 
- if (isset($_GET["select"])) {
+ if (isset($_GET["selected"])) {
    $selectErr = "Select invalido";
  }
 
  if (!(isset($_GET["s"]))) {
    $postgetErr = "Radio invalido";
  }
- if (!(isset($_GET["check"]))){
-     $check = "Check invalido";
- }
 }
 
-?>
-<?php echo "TESTE"; ?>
-<?php
 if($nameErr == $senhaErr && $postgetErr == $feedErr && $check == ""){
-    echo "Tudo certo";
+  echo '<script> alert(" Tudo certo:" + 
+  "\n Nome: " +' . $name . '+
+  "\n Senha: " +' . $senha . '+
+  "\n Feed: " +' . $feed . '+
+  "\n Select: " +' . $select . '
+  ) </script>';
 }
 else{
-    echo "O que esta invalido é: <br>" .
-    $nameErr . "<br>" .
-    $senhaErr . "<br>" .
-    $postgetErr . "<br>" .
-    $feedErr . "<br>" .
-    $check . "<br>";
-}?>
+    echo "<script> alert('O que esta invalido é: " .
+    $nameErr . " " .
+    $senhaErr . " " .
+    $postgetErr . " " .
+    $feedErr . " " .
+    $check . " ') </script>";
+}
+
+$myfile = fopen("autenticacao.txt", "r") or die("Unable to open file!");
+
+$id = fgets($myfile);
+$pass = fgets($myfile);
+
+(string)$pass = md5($pass);
+(string)$senha = md5($senha);
+
+if(strcmp($id,$name)){
+  if(!strcmp($pass,$senha)){
+    echo '<script> alert("Autenticacao com Sucesso") </script>';
+  }else{
+    echo '<script> alert("Autenticacao Falhou") </script>';
+  }
+}else{
+  echo '<script> alert("Autenticacao Falhou") </script>';
+}
+fclose($myfile);
 
 
-</body>
-</html>
+
+?>
