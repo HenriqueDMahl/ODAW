815a38c83bdc41e0f002594e2c5b706d734cf5ab
HenriqueDMahl <42521129+HenriqueDMahl@users.noreply.github.com>
2018-10-25 10:47:24 -0300

    Add files via upload

---
+++ TrabalhoFinal/PHP_HTML/telaInicial.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="../CSS/css_padrao.css">
+  </head>
+  
+  <body class = "body_registro">
+	<script src="../JAVASCRIPT/javascript.js"></script>
+	<div>
+	    <!-- Placeholder -->
+		<div class = "tela_inicial">
+			OI
+		</div>
+	</div>
+  
+  </body>
+</html>
