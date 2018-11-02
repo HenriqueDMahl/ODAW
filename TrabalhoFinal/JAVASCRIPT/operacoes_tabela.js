89a38c20bf3cfea29acaeab6ac79195da25bc33a
HenriqueDMahl <42521129+HenriqueDMahl@users.noreply.github.com>
2018-11-02 12:58:37 -0300

    Delete operacoes_tabela.js

--- TrabalhoFinal/JAVASCRIPT/operacoes_tabela.js
+++
@@ -1,8 +0,0 @@
-function pegaID(id) {
-	var arg = Number(id);
-	
-	//var lista = <?php echo select(arg); ?>
-	alert(arg);
- 
-}
-//https://stackoverflow.com/questions/15757750/how-can-i-call-php-functions-by-javascript
